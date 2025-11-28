a8936c249db696a9e49f9e239bf5df5a57b20336
Azamat-JS <azamatielts987@gmail.com>
2025-11-28 18:47:16 +0500

    implemented endpoints of users section

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,23 +1,54 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get()
+  @Get('all')
   getAllUsers() {
     return this.usersService.getAllUsers();
   }
 
-  @Get()
+  @Get('all/teachers')
   getAllTeachers() {
     return this.usersService.getAllTeachers();
   }
 
-  @Get()
+  @Get('all/assistants')
   getAllAssistants() {
     return this.usersService.getAllAssistants();
   }
   
+  @Post('create')
+  createUser() {
+  }
+
+  @Post('login')
+  loginUser() {
+  }
+  
+  @Post('logout')
+  logoutUser() {
+  }
+
+  @Post('forgotpassword')
+  forgotPassword() {
+  }
+
+  @Post('reset-password')
+  resetPassword() {
+  }
+
+  @Get('me')
+  getProfile() {
+  }
+
+  @Get('number')
+  getUserNumber() {
+  }
+
+  @Put('edit/:id')
+  editUser() {
+  }
 }
