56239a4faec29154f31db01498d7af9008f4c506
Azamat-JS <azamatielts987@gmail.com>
2025-12-01 23:20:38 +0500

    added fields in CreateModuleDto

--- packages/db/dtos/modules.dto.ts
+++ packages/db/dtos/modules.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional, IsString, IsUUID } from 'class-validator';
 
 export class CreateModuleDto {
     @IsNotEmpty()
@@ -8,6 +8,18 @@ export class CreateModuleDto {
     @IsNotEmpty()
     @IsString()
     status: string;
+
+    @IsOptional()
+    @IsNumber()
+    max_students: number;
+
+    @IsOptional()
+    @IsString()
+    is_core: string;
+
+    @IsNotEmpty()
+    @IsUUID()
+    course_id: string;
 
     @IsOptional()
     @IsNumber()
@@ -26,4 +38,16 @@ export class UpdateModuleDto {
     @IsOptional()
     @IsNumber()
     sequence?: number;
+
+    @IsOptional()
+    @IsNumber()
+    max_students?: number;
+
+    @IsOptional()
+    @IsString()
+    is_core?: string;
+
+    @IsOptional()
+    @IsUUID()
+    course_id?: string;
 }
